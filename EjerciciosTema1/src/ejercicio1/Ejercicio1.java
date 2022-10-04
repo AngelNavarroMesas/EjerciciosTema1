ccdb752ca3c712354f6651a2ac2164bfe171f344
anavarro <anavarro@106-18.info2>
2022-10-04 10:49:01 +0200

    commit comentarios

--- EjerciciosTema1/src/ejercicio1/Ejercicio1.java
+++ EjerciciosTema1/src/ejercicio1/Ejercicio1.java
@@ -7,13 +7,17 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaramos la variable para este ejercicio
 		double num;
 		
+		//Le pedimos al usuario que introduzca un numero y usamos el escaner par darle ese valor a la variable
 		System.out.println("Introduzca un numero con decimales");
 		num = sc.nextDouble();
 		
+		//Esta operacion nos permite redondear el numero sin usar Math.pow
 		num +=0.5;
 		
+		//Mostramos al usuario el redondeo del numero que ha introducido
 		System.out.println("numero redondeado " +(int)num);
 		
 		sc.close();
